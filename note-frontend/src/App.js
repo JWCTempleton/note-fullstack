8f15ada22e998f5c3d38d15bfd88e3195c684a1f
Jacob Templeton <jacob.templeton@gmail.com>
2023-02-27 16:17:54 -0800

    Add input to allow entering of new notes, state to store new notes

--- note-frontend/src/App.js
+++ note-frontend/src/App.js
@@ -1,14 +1,32 @@
 import "./App.css";
+import { useState } from "react";
+import Note from "./components/Note";
 
 function App({ notes }) {
+  const [allNotes, setAllNotes] = useState(notes);
+  const [newNote, setNewNote] = useState("a new note...");
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note) => (
-          <li key={note.id}>{note.content}</li>
+        {allNotes.map((note) => (
+          <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 }
