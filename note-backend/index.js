ac0d45254a3d2efb11ef84a899df8afdf633d9a6
Jacob Templeton <jacob.templeton@gmail.com>
2023-03-01 15:43:12 -0800

    Add change to POST request to return a 404 error if the content of the body is empty

--- note-backend/index.js
+++ note-backend/index.js
@@ -17,6 +17,11 @@ let notes = [
   },
 ];
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
 });
@@ -36,8 +41,22 @@ app.get("/api/notes/:id", (request, response) => {
 });
 
 app.post("/api/notes", (request, response) => {
-  const note = request.body;
-  console.log(note);
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
   response.json(note);
 });
 
