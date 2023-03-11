2cebae658c96465436f5e92e43bc7d33b0979f5d
Jacob Templeton <jacob.templeton@gmail.com>
2023-03-10 20:43:49 -0800

    Add test to determine if a specific note can be retrieved and viewed, add test to determine if a note can be deleted (verified working). Add change to make get by id request and delete request async functions (verified still working through tests)

--- note-backend/tests/note_api.test.js
+++ note-backend/tests/note_api.test.js
@@ -6,11 +6,6 @@ const app = require("../app");
 const api = supertest(app);
 const Note = require("../models/note");
 
-const initialNotes = [
-  { content: "HTML is easy", important: false },
-  { content: "Browser can execute only JavaScript", important: true },
-];
-
 beforeEach(async () => {
   await Note.deleteMany({});
   let noteObject = new Note(helper.initialNotes[0]);
@@ -71,6 +66,32 @@ test("note without content is not added", async () => {
   const notesAtEnd = await helper.notesInDb();
 
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length);
+});
+
+test("a specific note can be viewed", async () => {
+  const notesAtStart = await helper.notesInDb();
+
+  const noteToView = notesAtStart[0];
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+  expect(resultNote.body).toEqual(noteToView);
+});
+
+test("a note can be deleted", async () => {
+  const notesAtStart = await helper.notesInDb();
+  const noteToDelete = notesAtStart[0];
+
+  await api.delete(`/api/notes/${noteToDelete.id}`).expect(204);
+  const notesAtEnd = await helper.notesInDb();
+
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1);
+
+  const contents = notesAtEnd.map((r) => r.content);
+
+  expect(contents).not.toContain(noteToDelete.content);
 });
 
 afterAll(async () => {
