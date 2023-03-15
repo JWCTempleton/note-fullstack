c6734f55255d9fc9b5f5363528f4e6437aede898
Jacob Templeton <jacob.templeton@gmail.com>
2023-03-14 18:27:44 -0700

    Add state to handle users logging in, state to handle logged in users, and a form to submit username and password

--- note-frontend/src/App.js
+++ note-frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import Note from "./components/Note";
 import noteService from "./services/notes";
+import loginService from "./services/login";
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
 
@@ -10,6 +11,9 @@ function App({ notes }) {
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((response) => {
@@ -28,6 +32,24 @@ function App({ notes }) {
       setAllNotes(allNotes.concat(response));
       setNewNote("");
     });
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
   };
 
   const handleNoteChange = (event) => {
@@ -61,6 +83,27 @@ function App({ notes }) {
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
