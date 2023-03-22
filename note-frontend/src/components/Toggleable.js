bbb721b4a25674ae3c80dd5e1cc70e9473f18501
Jacob Templeton <jacob.templeton@gmail.com>
2023-03-22 15:45:20 -0700

    Add test for Toggleable content to ensure it is working as intended, tests pass

--- note-frontend/src/components/Toggleable.js
+++ note-frontend/src/components/Toggleable.js
@@ -15,7 +15,7 @@ const Toggleable = (props) => {
       <div style={hideWhenVisible}>
         <button onClick={toggleVisibility}>{props.buttonLabel}</button>
       </div>
-      <div style={showWhenVisible}>
+      <div style={showWhenVisible} className="toggleableContent">
         {props.children}
         <button onClick={toggleVisibility}>cancel</button>
       </div>
